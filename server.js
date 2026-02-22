056c466ea186ff9eaca4bf391af66a5ad0c7e0d2
paramsoni9339 <paramsoni.nmims@gmail.com>
2026-02-22 22:46:18 +0530

    27/02-2.4

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({ port:8080 });
-
-//connection event
+// Changed port from 8080 to 8081 here 👇
+const wss = new WebSocketServer({ port: 8081 });
 
 //ps notes: 0 means for connection , 1 is open , 2 means closing , 3 is closed
 
-
 wss.on('connection',(socket, request) => {
-    const ip= request.socket.remoteAddress;
+    const ip = request.socket.remoteAddress;
 
     socket.on('message', (rawData) => {
         const message = rawData.toString();
         console.log({rawData: rawData});
 
-
         wss.clients.forEach((client) => {
             if(client.readyState === WebSocket.OPEN) client.send(`Server Broadcast : ${message}`);
         })
     });
+    
     socket.on('error', (err) =>{
         console.error(`Error: ${err.message}: ${ip}`);
     })
@@ -26,6 +24,6 @@ wss.on('connection',(socket, request) => {
     socket.on('close', () => {
         console.log('client disconnected')
     })
-
-
 });
+
+console.log("Websocket server is liveeee yayayaya pn ws://localhost:8081");
